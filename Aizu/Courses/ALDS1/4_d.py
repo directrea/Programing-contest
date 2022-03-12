20fbff81407a4fca70fbe419d945020901e59f4b
shun <syunssssin@gmail.com>
2022-03-12 11:40:36 +0900

    探索の応用 完成 Rの初期値の桁を間違えていた

--- Aizu/Courses/ALDS1/4_d.py
+++ Aizu/Courses/ALDS1/4_d.py
@@ -1,3 +1,4 @@
+
 import io
 import sys
 _INPUT = """\
@@ -11,33 +12,35 @@ _INPUT = """\
 # sys.stdin = io.StringIO(_INPUT)
 
 
-N, TRACKS = map(int, input().split())
-loads = []
+N, K = map(int, input().split())
+W = []
 for _ in range(N):
-    loads.append(int(input()))
+    W.append(int(input()))
 
 
-def isCarry(max_carry):
-    loads_idx = 0
-    track = 0
-    while loads_idx < N and track < TRACKS:
+def isPack(P):
+    track_index = 0
+    w_index = 0
+    while w_index < N and track_index < K:
         tmp_sum = 0
-        while loads_idx < N and tmp_sum + loads[loads_idx] <= max_carry:
-            tmp_sum += loads[loads_idx]
-            loads_idx += 1
-        track += 1
-    return loads_idx == N
+        while w_index < N and tmp_sum + W[w_index] <= P:
+            tmp_sum += W[w_index]
+            w_index += 1
+        track_index += 1
+    return w_index == N
 
 
-right = 10000*10000
-left = 0
-mid = (left+right)//2
-ans = right
-while left <= right:
-    if isCarry(mid):
+# print(isPack(10))
+L = 0
+R = 100000*100000
+mid = (R+L)//2
+ans = R
+
+while L <= R:
+    if isPack(mid):
         ans = mid
-        right = mid - 1
+        R = mid - 1
     else:
-        left = mid + 1
-    mid = (left+right)//2
+        L = mid + 1
+    mid = (L+R)//2
 print("%d" % ans)
