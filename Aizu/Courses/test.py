bb9d5884ede5d244b418b123f0659e657ea5baca
shun <syunssssin@gmail.com>
2022-03-28 22:18:39 +0900

    フィボナッチ数列

--- Aizu/Courses/test.py
+++ Aizu/Courses/test.py
@@ -1,1 +1,17 @@
-print(2 // 2)
+def binary_brute_force(n):
+    A = [0] * n
+    rec(A, 0)
+
+
+def rec(A, i):
+    if i < len(A):
+        A[i] = 0
+        rec(A, i + 1)
+        print(A)
+        A[i] = 1
+        rec(A, i + 1)
+        print(A)
+
+
+N = 2
+binary_brute_force(N)
