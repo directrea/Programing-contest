b3793a2c48d4c3a66323e16cf115692e3d3781b4
shun <syunssssin@gmail.com>
2022-03-29 16:44:20 +0900

    メモ化再帰と動的計画法の違い

--- Aizu/Courses/ALDS1/10_A_フィボナッチ数列.py
+++ Aizu/Courses/ALDS1/10_A_フィボナッチ数列.py
@@ -4,16 +4,29 @@ import sys
 _INPUT = """\
 44
 """
-# sys.stdin = io.StringIO(_INPUT)
+sys.stdin = io.StringIO(_INPUT)
 
 N = int(input())
 A = [1, 1]
 
 
-def Fibonacchi(N):
+def dynamicFibonacchi(N):
     for i in range(2, N + 1):
         A.append(A[i - 1] + A[i - 2])
     return A[N]
 
 
-print(Fibonacchi(N))
+memo_A = [0] * (N + 1)
+
+
+def memofibonacci(N):
+    if 0 <= N and N == 0 or N == 1:
+        memo_A[N] = 1
+    if memo_A[N]:
+        return memo_A[N]
+    num = memofibonacci(N - 1) + memofibonacci(N - 2)
+    memo_A[N] = num
+    return num
+
+
+print(memofibonacci(N))
