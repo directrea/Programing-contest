fd6cb75106f42178278c62dbf68dab7b7c96e02c
shun <syunssssin@gmail.com>
2022-04-11 09:16:59 +0900

    pair 使ってみた

--- AtCoder/Beginner Contest247/D.cpp
+++ AtCoder/Beginner Contest247/D.cpp
@@ -7,28 +7,26 @@ using ll = long long;
 int main() {
   int N;
   cin >> N;
-  deque<ll> val;
-  deque<ll> amount;
+  deque<pair<ll, ll>> dq;  // pair<値, 個数>
   for (int i = 0; i < N; i++) {
     int query, x, c;
     cin >> query;
     switch (query) {
       case 1:
         cin >> x >> c;
-        val.push_back(x);
-        amount.push_back(c);
+        dq.push_back({x, c});
         break;
       case 2:
         cin >> c;
         ll ans_sum = 0;
         while (c) {
-          if (c >= amount.front()) {
-            c -= amount.front();
-            ans_sum += val.front() * amount.front();
-            val.pop_front(), amount.pop_front();
+          if (c >= dq.front().second) {
+            c -= dq.front().second;
+            ans_sum += dq.front().first * dq.front().second;
+            dq.pop_front();
           } else {
-            amount[0] -= c;
-            ans_sum += val.front() * c;
+            dq.front().second -= c;
+            ans_sum += dq.front().first * c;
             c = 0;
           }
         }
