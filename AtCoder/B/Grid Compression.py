1bbce2574d0cf39d266afab43f1b237873ffaaad
shun <syunssssin@gmail.com>
2022-04-28 00:37:49 +0900

    a

--- AtCoder/B/Grid Compression.py
+++ AtCoder/B/Grid Compression.py
@@ -1,18 +1,27 @@
 import io
 import sys
+from collections import deque
 
 _INPUT = """\
-4 4
-##.#
-....
-##.#
-.#.#
+4 5
+.....
+.....
+..#..
+.....
 
 """
 sys.stdin = io.StringIO(_INPUT)
 
-H, W = map(int, input().split())
 
+def isRowblank(vec):
+    bol = True
+    for i in vec:
+        if (i):
+            bol = False
+    return bol
+
+
+H, W = map(int, input().split())
 vvec = [[False for _ in range(W)] for _ in range(H)]
 
 for i in range(H):
@@ -20,5 +29,33 @@ for i in range(H):
     for j in range(W):
         if tmp[j] == '#':
             vvec[i][j] = True
+idx = 0
+rvvec = []
+for i in range(len(rvvec)):
+    if (isRowblank(rvvec[i])):
+        pass
+    else:
+        rvvec.append(rvvec[i])
 
-pass
+blankcols = []
+for j in range(len(rvvec[0])):
+    isblankcol = True
+    for i in range(len(rvvec)):
+        if (rvvec[i][j]):
+            isblankcol = False
+    if (isblankcol):
+        blankcols.append(j)
+
+deledcnt = 0
+for col in blankcols:
+    for i in range(len(rvvec)):
+        del rvvec[i][col - deledcnt]
+    deledcnt += 1
+
+for vec in rvvec:
+    for b in vec:
+        if b:
+            print("#", end="")
+        else:
+            print(".", end="")
+    print()
--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-import io
-import sys
-_INPUT = """\
+vvec = [[0 for _ in range(3)] for _ in range(3)]
+vvec[1][0] = 1
 
-"""
-sys.stdin = io.StringIO(_INPUT)
-N = 3
-for bit in range(1 << N):
-    tmp = []
-    for i in range(N):
-        if bit & (1 << i):
-            tmp.append(i)
-    print(tmp)
-=======
-print(ord('a'))
->>>>>>> 22c495ff0393198b13b7c03f135763c377f06392
+del vvec[1]
+pass
