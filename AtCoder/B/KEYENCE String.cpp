b1ebbfd7405c6a9f815010b37aa7cac709e7cde1
shun <syunssssin@gmail.com>
2022-05-07 22:46:51 +0900

    できないです

--- AtCoder/B/KEYENCE String.cpp
+++ AtCoder/B/KEYENCE String.cpp
@@ -7,15 +7,42 @@ using namespace std;
 int main() {
   string str;
   cin >> str;
+  int n = str.size();
   string key = "keyence";
-  int idx = 0;
-  for (char c : str) {
-    if (key[idx] == c) idx++;
-    if (idx == key.size()) break;
+
+  for (int i = 0; i < n; i++) {
+    int idx = 0;
+    string ansstr = "";
+    if (str[i] == key[0]) {
+      while (str[i] == key[idx]) {
+        if (ansstr == key) {
+          cout << "YES" << endl;
+          return 0;
+        }
+        ansstr += key[idx];
+        i++, idx++;
+      }
+
+      // idx++;
+      for (int j = i + 1; j < n; j++) {
+        string secansstr = ansstr;
+        int secidx = idx;
+        int tmpj = j;
+        while (str[tmpj] == key[secidx]) {
+          if (secansstr == key) {
+            cout << "YES" << endl;
+            return 0;
+          }
+          secansstr += key[secidx];
+          tmpj++, secidx++;
+        }
+        if (secansstr == key) {
+          cout << "YES" << endl;
+          return 0;
+        }
+      }
+    }
   }
-  if (idx == key.size())
-    cout << "YES" << endl;
-  else
-    cout << "NO" << endl;
+  cout << "NO" << endl;
   return 0;
 }
