9c6c41f0cefbc3f585697e206fef4dfcac876a65
shun <syunssssin@gmail.com>
2022-05-13 19:31:40 +0900

    a

--- test.cpp
+++ test.cpp
@@ -5,10 +5,6 @@ using ll = long long;
 using namespace std;
 
 int main() {
-  for (int i = -1; i <= 1; i++) {
-    for (int j = -1; j <= 1; j++) {
-      if (i == 0 ^ j == 0) cout << i << " " << j << endl;
-    }
-  }
+  cout << !false << endl;
   return 0;
 }
