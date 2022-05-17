c4eb1ac9c524a02056cff767526d4d297da3558d
shun <syunssssin@gmail.com>
2022-05-17 22:34:05 +0900

    意外と簡単だった

--- AtCoder/B/KEYENCE String.cpp
+++ AtCoder/B/KEYENCE String.cpp
@@ -6,10 +6,31 @@ using namespace std;
 
 int main() {
   string str;
+  // str = "keyofscience";
   cin >> str;
-  int n = str.size();
   string key = "keyence";
-  cout << n - key.size() << endl;
 
+  int N = str.size();
+  int top = key.size(), tail = N - 1;
+  while (top >= 0) {
+    string ptop = "";
+    for (int i = 0; i < top; i++) {
+      ptop += str[i];
+    }
+    // cout << ptop << " ";
+    string ptail = "";
+    for (int i = tail + 1; i < N; i++) {
+      ptail += str[i];
+    }
+    // cout << ptail << " ";
+    // cout << ptop + ptail << endl;
+    if (ptop + ptail == key) {
+      cout << "YES" << endl;
+      return 0;
+    }
+
+    top--, tail--;
+  }
+  cout << "NO" << endl;
   return 0;
 }
