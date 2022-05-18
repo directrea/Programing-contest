5c6afd88c571fbd402352c623e72b2fa90340046
shun <syunssssin@gmail.com>
2022-05-18 23:05:51 +0900

    aaaa

--- AtCoder/C/Bugged.cpp
+++ AtCoder/C/Bugged.cpp
@@ -9,21 +9,24 @@ int main() {
   cin >> N;
   vector<int> vec(N);
   rep(i, 0, N) cin >> vec[i];
-  sort(all(vec));
-  int maxscore = accumulate(all(vec), 0);
+  // sort(all(vec));
   // cout << endl;
-
+  vector<int> ansvec;
   for (int i = 0; i < (1 << N); i++) {
     int tmpsum = 0;
     for (int bit = 0; bit < N; bit++) {
       if (i & (1 << bit)) tmpsum += vec[bit];
     }
-    int ans = maxscore - tmpsum;
-    if (ans % 10 != 0) {
-      cout << ans << endl;
+    ansvec.push_back(tmpsum);
+  }
+  sort(all(ansvec), greater<int>());
+  for (int i : ansvec) {
+    if (i % 10 != 0) {
+      cout << i << endl;
       return 0;
     }
   }
   cout << 0 << endl;
+
   return 0;
 }
