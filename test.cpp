ecb5badad459d4cc92a441dd30b767a545caf9cc
shun <syunssssin@gmail.com>
2022-05-22 00:21:30 +0900

    環境構築しなおしました

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using namespace std;
+
+int main() {
+  cout << "a" << endl;
+  
+  return 0;
+}
